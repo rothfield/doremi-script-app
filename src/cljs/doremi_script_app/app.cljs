c78aee282efc68e2d1a2dc87f66f8412672b8047
John Rothfield <rthfield@sonic.net>
2015-03-20 19:58:49 -0700

    good progress.

--- src/cljs/doremi_script_app/app.cljs
+++ src/cljs/doremi_script_app/app.cljs
@@ -1,28 +1,20 @@
 (ns doremi-script-app.app
-
-  ;;(:require-macros [cljs.core.async.macros :refer [go]])
-  (:require-macros [cljs.core :refer [assert]])
+  ;;(:require-macros 
+  (:require-macros [cljs.core :refer [assert]]
+                   ;;  [cljs.core.async.macros :refer [go]]
+                   )
   (:require 
-    [doremi-script-app.doremi_core :as doremi_core :refer [doremi-text->collapsed-parse-tree]]
-    [goog.string :as gstring]
+    [doremi-script-app.doremi_core :as doremi_core
+     :refer [doremi-text->collapsed-parse-tree]]
+    ;;  [goog.string :as gstring]
     [goog.net.XhrIo :as xhr]
-    [cljs.core.async :refer [<! chan close!]]
+    ;;  [cljs.core.async :refer [<! chan close!]]
     [reagent.core :as reagent :refer [atom]]
     [instaparse.core :as insta] 
     ))
 
 (declare draw-item)
-(def lookup-barline
-  {
-   "single-barline" "&#x1d100",
-   "double-barline" "&#x1d101",
-   "left-repeat" "&#x1d106",
-   "mordent" "&#x1D19D&#x1D19D",
-   "right-repeat" "&#x1d107",
-   "final-barline" "&#x1d102",
-   "reverse-final-barline" "&#x1d103"
-   }
-  )
+
 (defn is-a[s v]
   assert(and (vector? v) (= (first v) s))
   )
@@ -37,6 +29,202 @@
   (dorun (map 
            #(my-log (.stringify js/JSON (clj->js %)))
            my-args)))
+
+
+
+;;;;     
+;;;;     . The Unicode character ♭(U+266D) is the flat sign. Its HTML entity is &#9837;.
+;;;;    In Unicode, the sharp symbol (♯) is at code point U+266F. Its HTML entity is &#9839;. The symbol for double sharp (double sharp) is at U+1D12A (so &#119082;). These characters may not display correctly in all fonts.
+;;;;    */
+(def sharp-symbol "&#9839;")
+(def flat-symbol  "&#9837;")
+(def lookup_simple {
+                    "b" "b"
+                    "#" "#"
+                    "." "&bull;"
+                    "*" "&bull;"
+                    "|:" "|:"
+                    "~" "~"
+                    ":|" ":|"
+                    "|" "|"
+                    "||" "||"
+                    "%" "%"
+                    "|]" "|"
+                    "[|" "|"
+                    })
+;;;;        return LOOKUP[str];
+;;;;      };
+;;;;      if (false) {
+;;;;        console.log(lookup_simple)
+;;;;      }
+(def lookup_html_entity {
+                         "b" "&#9837;"
+                         "#" "&#9839;"
+                         "." "&bull;"
+                         "*" "&bull;"
+                         "|:" "&#x1d106"
+                         "~" "&#x1D19D&#x1D19D"
+                         ":|" "&#x1d107"
+                         "|" "&#x1d100"
+                         "||" "&#x1d101"
+                         "%" "&#x1d10E"
+                         "|]" "&#x1d102"
+                         "[|" "&#x1d103"
+                         })
+(def lookup1 {
+              "Cb" ["S", flat-symbol]
+              "C" ["S"]
+              "C#" ["S", sharp-symbol]
+              "Db" ["r"]
+              "D" ["R"]
+              "D#" ["R", sharp-symbol]
+              "Eb" ["g"]
+              "E" ["G"]
+              "E#" ["G", sharp-symbol]
+              "F" ["m"]
+              "F#" ["M"]
+              "Gb" ["P", flat-symbol]
+              "G" ["P"]
+              "G#" ["P", sharp-symbol]
+              "Ab" ["d"]
+              "A" ["D"]
+              "A#" ["D", sharp-symbol]
+              "Bb" ["n"]
+              "B" ["N"]
+              "B#" ["N", sharp-symbol]
+              })
+(def lookup-number  {
+                     "Cb" ["1", flat-symbol]
+                     "C" ["1"]
+                     "C#" ["1", sharp-symbol]
+                     "Db" ["2", flat-symbol]
+                     "D" ["2"]
+                     "D#" ["2", sharp-symbol]
+                     "Eb" ["3", flat-symbol]
+                     "E" ["3"]
+                     "E#" ["3", sharp-symbol]
+                     "F" ["4"]
+                     "F#" ["4", sharp-symbol]
+                     "Gb" ["5", flat-symbol]
+                     "G" ["5"]
+                     "G#" ["5", sharp-symbol]
+                     "Ab" ["6", flat-symbol]
+                     "A" ["6"]
+                     "A#" ["6", sharp-symbol]
+                     "Bb" ["7", flat-symbol]
+                     "B" ["7"]
+                     "B#" ["7", sharp-symbol]
+                     })
+(def lookup-hindi 
+  ;; TODO
+  (let [ s  "&#x0938" ;;,"&#x1d100"u+938" "र"
+        r "r"
+        g "g";; "ग़"
+        m  "m" ;; "म"
+        p  "p" ;; "प"
+        d  "d" ;;"ध"
+        n  "n" ;; "ऩ"
+        tick  "'"]
+    {
+     "Cb"(str s  flat-symbol)
+     "C" s
+     "C#"(str s  sharp-symbol)
+     "Db" r
+     "D" r
+     "D#" (str r sharp-symbol)
+     "Eb" g
+     "E" g
+     "E#" (str g  sharp-symbol)
+     "F" m
+     "F#" (str m  tick)
+     "Gb" (str p  flat-symbol)
+     "G" p
+     "G#" (str p sharp-symbol)
+     "Ab" d
+     "A" d
+     "A#" (str d  sharp-symbol)
+     "Bb" n
+     "B" n
+     "B#" (str n  sharp-symbol)
+     }))
+(def lookup-ABC {
+                 "Cb" ["C", flat-symbol]
+                 "C" ["C"]
+                 "C#" ["C", sharp-symbol]
+                 "Db" ["D", flat-symbol]
+                 "D" ["D"]
+                 "D#" ["D", sharp-symbol]
+                 "Eb" ["E", flat-symbol]
+                 "E" ["E"]
+                 "E#" ["E", sharp-symbol]
+                 "F" ["F"]
+                 "F#" ["F", sharp-symbol]
+                 "Gb" ["G", flat-symbol]
+                 "G" ["G"]
+                 "G#" ["G", sharp-symbol]
+                 "Ab" ["A", flat-symbol]
+                 "A" ["A"]
+                 "A#" ["A", sharp-symbol]
+                 "Bb" ["B", flat-symbol]
+                 "B" ["B"]
+                 "B#" ["B", sharp-symbol]
+                 })
+
+(def lookup-DoReMi {
+                    "Cb" ["D", flat-symbol]
+                    "C" ["D"]
+                    "C#" ["D", sharp-symbol]
+                    "Db" ["R", flat-symbol]
+                    "D" ["R"]
+                    "D#" ["R", sharp-symbol]
+                    "Eb" ["M", flat-symbol]
+                    "E" ["M"]
+                    "E#" ["M", sharp-symbol]
+                    "F" ["F"]
+                    "F#" ["F", sharp-symbol]
+                    "Gb" ["S", flat-symbol]
+                    "G" ["S"]
+                    "G#" ["S", sharp-symbol]
+                    "Ab" ["L", flat-symbol]
+                    "A" ["L"]
+                    "A#" ["L", sharp-symbol]
+                    "Bb" ["T", flat-symbol]
+                    "B" ["T"]
+                    "B#" ["T", sharp-symbol]
+                    })
+
+(defn deconstruct-pitch-string-by-kind[pitch kind]
+  (log "render-pitch-in-kind")
+  (log "pitch is")
+  (log pitch)
+  (case kind
+    "sargam-composition"
+    (get lookup1 pitch)
+    "number-composition"
+    (get lookup-number pitch)
+    "abc-composition"
+    (get lookup-ABC pitch)
+    "doremi-composition"
+    (get lookup-DoReMi pitch)
+    "hindi-composition"
+    (get lookup-hindi pitch)
+    ;;default:
+    (get lookup1 pitch)
+    )
+  )
+
+
+(def lookup-barline
+  {
+   "single-barline" "&#x1d100",
+   "double-barline" "&#x1d101",
+   "left-repeat" "&#x1d106",
+   "mordent" "&#x1D19D&#x1D19D",
+   "right-repeat" "&#x1d107",
+   "final-barline" "&#x1d102",
+   "reverse-final-barline" "&#x1d103"
+   }
+  )
 
 
 (defonce app-state 
@@ -100,7 +288,7 @@
   [:div.stave.sargam_line
    (map-indexed
      (fn notes-line-aux[idx item]
-       ^{:key idx }(draw-item item))
+       (draw-item item idx))
      (rest item))
 
    ])
@@ -140,7 +328,7 @@
   [:span.beat.looped
    (map-indexed
      (fn beat-aux[idx item]
-       ^{:key idx }(draw-item item)
+       (draw-item item idx)
        )
      (rest item))
    ]
@@ -223,25 +411,68 @@
 
 
 (defn pitch-name[item]
-  [:span.note.pitch 
-   (second item)
+  (assert (is-a "pitch-name" item))
+  (log "pitch-name, item is")
+  (log item)
+  (log (second item))
+  [:span.note.pitch {:dangerouslySetInnerHTML  
+                     {
+                      :__html 
+                      (second item)
+                      }
+                     }
    ]
   )
+
+(defn pitch-alteration[item]
+  (log "pitch-alteration") 
+  (assert (is-a "pitch-alteration" item))
+  [:span.note.pitch.alteration 
+   {:dangerouslySetInnerHTML
+    {
+     :__html 
+     (second item)
+     }
+    }
+   ]
+  ) 
 
 (defn pitch[item]
   ;;; ["pitch","C#",["octave",1],["syl","syl"]]
   (log "pitch") 
   (assert (is-a "pitch" item))
   (log item)
-  (let [item2
-        (into[] (cons ["pitch-name" (second item)] (rest (rest item))))
+  ;; need to sort attributes in order:
+  ;; ornament octave syl note alteration
+  (let [
+        deconstructed-pitch ;; C#,sargam -> ["S" "#"] 
+        (deconstruct-pitch-string-by-kind (second item)
+                                          "sargam-composition" 
+                                          ) 
+        sort-table 
+        {"ornament" 1 
+         "octave" 2 
+         "syl" 3 
+         "pitch-name" 4 
+         "pitch-alteration" 5}
+        item-a
+        (into[] (cons ["pitch-name" (first deconstructed-pitch)]
+                      (rest (rest item))))
+        alteration-string (second deconstructed-pitch)
+        my-pitch-alteration (when alteration-string
+                              ["pitch-alteration" alteration-string])
+
+        item-b 
+        (into[] (cons my-pitch-alteration item-a))
+        item2 (sort-by #(get sort-table (first %)) item-b)
         ]
     (log "item2 is")
+    ;;[["pitch-name","D#"],["octave",1],["syl","syl"]] 
     (log item2)
     [:span.note_wrapper
      (map-indexed
        (fn pitch-aux[idx item]
-         {:key idx } (draw-item item)
+         (draw-item item idx)
          )
        item2)
      ]
@@ -281,7 +512,7 @@
   [:span {:class "measure"} 
    (map-indexed
      (fn measure-aux[idx item]
-       {:key idx } (draw-item item)
+       (draw-item item idx)
        )
      (rest item))
    ]
@@ -291,45 +522,45 @@
   (assert (is-a "syl" item))
   (log "syl- item is")
   (log item)
-  [:span.upper_octave_1.upper_octave_indicator
-   "•"]
+  [:span.syl (second item)]
   )
 
 
 (defn octave[item]
-  (assert (is-a "octave" item))
   (log "octave- item is")
-  (log "TODO")
   (log item)
+  (assert (is-a "octave" item))
   [:span.upper_octave_1.upper_octave_indicator
    "•"]
   )
 
 
-(defn draw-item[item]
+(defn draw-item[item idx]
   (let [my-key (first item)]
     (log "draw-item, item is")
     (log item)
     (case my-key
+      "pitch-alteration"
+      ^{:key idx} [pitch-alteration  item]
       "syl"
-      [syl item]
+      ^{:key idx} [syl  item]
       "octave"
-      [octave item]
+     ^{:key idx}  [octave  item]
       "pitch-name"
-      [pitch-name item]
+      ^{:key idx} [pitch-name  item]
       "measure"
-      [measure item]
+      ^{:key idx} [measure  item]
       "notes-line"
-      [notes-line item]
+      ^{:key idx} [notes-line  item]
       "stave"
-      [stave item]
+      ^{:key idx} [stave  item]
       "beat"
-      [beat item]
+      ^{:key idx} [beat  item]
       "pitch"
-      [pitch item]
+      ^{:key idx} [pitch  item]
       "dash"
-      [line-item { :kind "dash" :src "-"}]
-      [:span 
+      ^{:key idx} [line-item { :kind "dash" :src "-"}]
+      ^{:key idx} [:span 
        (str "todo-draw-item" (.stringify js/JSON (clj->js item)))
        ]
       )))
@@ -501,24 +732,26 @@
       [:img#staff_notation
        :name "",
        :src "/images/blank.png?1426699590838"]]]]
-   [composition (get-in @app-state [:parse-results :parsed])
-    ]
-   [:h3 "unit tests"]
-   [:h4 "stave"]
-   [stave ["stave",["notes-line",["measure",["beat",["pitch","C",["octave",0]]]]]]]
-   [:div.sargam_line
-    [beat ["beat",["pitch","C",["octave",0]],["dash"],["dash"]]]]
-   [lyrics-section ["lyrics-line","he-","llo","john"]] 
-   [:div.sargam_line [pitch ["pitch","C",["octave",0]]]]
-   [:div [:br]]
-   [:div.sargam_line [pitch ["pitch","C#",["octave",1],["syl","syl"]]]]
-   [:span "barline follows"]
-   [:div
-    {:dangerouslySetInnerHTML {:__html 
-                               (str "barline here?" (get lookup-barline "double-barline"))
-                               }
-     }
-    ] 
+   ;; [composition (get-in @app-state [:parse-results :parsed])
+   ;; ]
+   [:h3 "unit4
+        tests"]
+   [:div.sargam_line [pitch ["pitch","D#",["octave" 2]["syl""syl"]]]]
+   ;;;;    [:h4 "stave"]
+   ;;;;    [stave ["stave",["notes-line",["measure",["beat",["pitch","C",["octave",0]]]]]]]
+   ;;;;    [:div.sargam_line
+   ;;;;     [beat ["beat",["pitch","C",["octave",0]],["dash"],["dash"]]]]
+   ;;;;    [lyrics-section ["lyrics-line","he-","llo","john"]] 
+   ;;;;    [:div.sargam_line [pitch ["pitch","C",["octave",0]]]]
+   ;;;;    [:div [:br]]
+   ;;;;    [:div.sargam_line [pitch ["pitch","C#",["octave",1],["syl","syl"]]]]
+   ;;;;    [:span "barline follows"]
+   ;;;;    [:div
+   ;;;;     {:dangerouslySetInnerHTML {:__html 
+   ;;;;                                (str "barline here?" (get lookup-barline "double-barline"))
+   ;;;;                                }
+   ;;;;      }
+   ;;;;     ] 
    ]
   )
 
